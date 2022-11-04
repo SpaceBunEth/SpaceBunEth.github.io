4bac6fcbdd964c853e5cbba011186dc34e207a45
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-11-04 23:59:23 +0000

    react app hello world for new dev branch

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is up and running!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
