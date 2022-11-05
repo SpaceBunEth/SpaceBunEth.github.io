56afa6993499c764f7c3081149af8a27284c26bd
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-11-05 01:20:08 +0000

    basic bootstrap templates added to react webpage, basic boilerplate wireframe outline

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,166 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <main>
+      <h1 className='text-center bg-dark text-white m-0'>SPACEBUN LOGO SPACEBUN</h1>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">SpaceBun Logo</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample03">
+        <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled">Disabled</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form role="search">
+          <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+        </form>
+      </div>
+    </div>
+  </nav>
+ 
+{/* start of carousel */}
+  <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" className="active" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" className=""></button>
+    </div>
+    <div className="carousel-inner">
+      <div className="carousel-item">
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+        <div className="container">
+          <div className="carousel-caption text-start">
+            <h1>Example headline.</h1>
+            <p>Some representative placeholder content for the first slide of the carousel.</p>
+            <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+          </div>
+        </div>
+      </div>
+      <div className="carousel-item active">
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+        <div className="container">
+          <div className="carousel-caption">
+            <h1>Another example headline.</h1>
+            <p>Some representative placeholder content for the second slide of the carousel.</p>
+            <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
+          </div>
+        </div>
+      </div>
+      <div className="carousel-item">
+        <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+
+        <div className="container">
+          <div className="carousel-caption text-end">
+            <h1>One more for good measure.</h1>
+            <p>Some representative placeholder content for the third slide of this carousel.</p>
+            <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+{/* Start of body elements */}
+  <div className="container marketing">
+
+    {/* <!-- Three columns of text below the carousel --> */}
+    <div className="row m-5">
+      <div className="col-lg-4">
+        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2 className="fw-normal">Heading</h2>
+        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+        <p><a className="btn btn-secondary" href="#">View details »</a></p>
+      </div>
+      {/* <!-- /.col-lg-4 --> */}
+      <div className="col-lg-4">
+        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2 className="fw-normal">Heading</h2>
+        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+        <p><a className="btn btn-secondary" href="#">View details »</a></p>
+      </div>
+      {/* <!-- /.col-lg-4 --> */}
+      <div className="col-lg-4">
+        <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+
+        <h2 className="fw-normal">Heading</h2>
+        <p>And lastly this, the third column of representative placeholder content.</p>
+        <p><a className="btn btn-secondary" href="#">View details »</a></p>
+      </div>
+      {/* <!-- /.col-lg-4 --> */}
+    </div>
+    {/* <!-- /.row --> */}
+
+
+    {/* <!-- START THE FEATURETTES --> */}
+
+    <hr className="featurette-divider"/>
+
+    <div className="row featurette">
+      <div className="col-md-7 order-md-2">
+        <h2 className="featurette-heading fw-normal lh-1">Oh yeah, it's that good. <span className="text-muted">See for yourself.</span></h2>
+        <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+      </div>
+      <div className="col-md-5 order-md-1">
+        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+      </div>
+    </div>
+    {/* <!-- /END THE FEATURETTES --> */}
+
+  </div>
+    {/* Start of Footer */}
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+
+        <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          <svg className="bi me-2" width="40" height="32"></svg>
+        </a>
+
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
+          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Features</a></li>
+          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Pricing</a></li>
+          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+          <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+        </ul>
+      </footer>
+    </div>
+    </main>
   );
 }
 
